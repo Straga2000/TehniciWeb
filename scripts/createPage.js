5ca8533e7d23e744ee830aad11312d6ca55a7afd
Bogdan Gusuleac <bgusuleac@gmail.com>
2020-05-29 01:48:15 -0700

    Ceva

---
+++ scripts/createPage.js
@@ -0,0 +1,130 @@
+var priceValue = null;
+var nameValue = null;
+var counter = 0;
+
+var countProducts = 0;
+
+window.onload = function () {
+    setInterval(changeTitle, 3000);
+    getBook();
+};
+
+function changeTitle() {
+    try {
+        var titleElement = document.getElementsByTagName("h1")[0];
+
+        if(counter % 2 === 1)
+        {
+            titleElement.innerText = nameValue;
+            titleElement.style.color = '#ffffff';
+            titleElement.style.fontWeight = "lighter";
+        }
+        else
+        {
+            titleElement.innerText = priceValue;
+            titleElement.style.color = '#0ac6fc';
+            titleElement.style.fontWeight = "bold";
+        }
+
+        //console.log(titleElement);
+        counter += 1;
+    }
+    catch (e)
+    {
+    }
+}
+
+/*function addProduct(JSONData) {
+    idBookPage = localStorage.getItem("idBookPage");
+    $.ajax({
+        url: 'http://localhost:3000/data/1',
+        type: 'PUT',
+        data: JSON.stringify(JSONData),
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(data) {
+            console.log("Nu s a reusit");
+        }
+    });
+}*/
+
+function getBook() {
+    idBookPage = localStorage.getItem("idBookPage");
+    $.ajax({
+        url: 'http://localhost:3000/book',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            createPage(data[idBookPage])
+            /*for(let i = 0; i < data.length; i++)
+                if(data.id === idBookPage)
+                {
+                    createPage(data[i]);
+                    console.log(data[i]);
+                    break;
+                }*/
+            /*console.log(idBookPage, data[idBookPage]);*/
+        },
+        error: function(data) {
+            console.log("Nu s a reusit");
+        }
+    });
+}
+
+function createPage(object) {
+
+    var parentElement = document.getElementsByClassName("book")[0];
+
+    /*Title part here*/
+    var titleElement = document.createElement('h1');
+    titleElement.classList.add("title", "page", "font-x6");
+    titleElement.innerText = object.title;
+
+    priceValue = "Just " + (object.price).toString() + "$";
+    nameValue = object.title;
+    parentElement.appendChild(titleElement);
+
+    /*Container part here*/
+    var container = document.createElement("div");
+    container.className = "line";
+
+    /*Text part here*/
+    var description = document.createElement("p");
+
+    if(!(object.shortDescription === undefined))
+        description.innerText = object.shortDescription;
+
+    if (!(object.longDescription === undefined))
+        description.innerText = object.longDescription;
+
+    /*Image container*/
+    var imageContainer = document.createElement("div");
+    imageContainer.className = "image";
+
+    /*Image*/
+    var image = document.createElement("img");
+    image.src = object.thumbnailUrl;
+
+    /*Button*/
+    var button = document.createElement("button");
+    button.className = "btn";
+    button.innerText = "Add to cart";
+
+    button.onclick = function (){
+
+        var text = '{'  + "\""+ countProducts.toString() + "\""+' : ' + object.id.toString() + '}';
+        console.log(text);
+        addProduct(JSON.parse(text)); countProducts++;
+    };
+
+    /*Combine all*/
+    imageContainer.appendChild(image);
+
+    container.appendChild(description);
+    container.appendChild(imageContainer);
+
+    parentElement.appendChild(titleElement);
+    parentElement.appendChild(container);
+    parentElement.appendChild(button);
+}
---
+++ scripts/getBooks.js
@@ -0,0 +1,210 @@
+var newBook = {
+    title: "My happy book",
+    isbn: "161729243",
+    pageCount: 0,
+    thumbnailUrl: "images/newBook.jpg",
+    shortDescription: "It's a happy book!" ,
+    price: 1234
+};
+
+/*  "shortDescription": "Unlocking Android: A Developer's Guide provides concise, hands-on instruction for the Android operating system and development tools. This book teaches important architectural concepts in a straightforward writing style and builds on this with practical and useful examples throughout.",*/
+var newUpdateBook = {
+    title: "Unlocking Android",
+    isbn: "1933988673",
+    pageCount: 416,
+    publishedDate: {
+        "$date": "2009-04-01T00:00:00.000-0700"
+    },
+    thumbnailUrl: "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg",
+    shortDescription: "Good joke",
+    longDescription: "Android is an open source mobile phone platform based on the Linux operating system and developed by the Open Handset Alliance, a consortium of over 30 hardware, software and telecom companies that focus on open standards for mobile devices. Led by search giant, Google, Android is designed to deliver a better and more open and cost effective mobile experience.    Unlocking Android: A Developer's Guide provides concise, hands-on instruction for the Android operating system and development tools. This book teaches important architectural concepts in a straightforward writing style and builds on this with practical and useful examples throughout. Based on his mobile development experience and his deep knowledge of the arcane Android technical documentation, the author conveys the know-how you need to develop practical applications that build upon or replace any of Androids features, however small.    Unlocking Android: A Developer's Guide prepares the reader to embrace the platform in easy-to-understand language and builds on this foundation with re-usable Java code examples. It is ideal for corporate and hobbyists alike who have an interest, or a mandate, to deliver software functionality for cell phones.    WHAT'S INSIDE:        * Android's place in the market      * Using the Eclipse environment for Android development      * The Intents - how and why they are used      * Application classes:            o Activity            o Service            o IntentReceiver       * User interface design      * Using the ContentProvider to manage data      * Persisting data with the SQLite database      * Networking examples      * Telephony applications      * Notification methods      * OpenGL, animation & multimedia      * Sample Applications  ",
+    status: "PUBLISH",
+    authors: [
+        "W. Frank Ableson",
+        "Charlie Collins",
+        "Robi Sen"
+    ],
+    categories: [
+        "Open Source",
+        "Mobile"
+    ],
+    id: 0,
+    price: 33.01
+};
+
+window.onload = function () {
+
+    getStock();
+
+    var deleteButton = document.getElementById("delete");
+    var updateButton = document.getElementById("update");
+    var createButton = document.getElementById("create");
+    var getButton = document.getElementById("get");
+
+    deleteButton.onclick = deleteBook;
+    createButton.onclick = createBook;
+    getButton.onclick = getRandomBook;
+    updateButton.onclick = updateBook;
+
+};
+
+function getRandomBook() {
+    $.ajax({
+        url: 'http://localhost:3000/book',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var rnd = Math.round(data.length * Math.random()).toString();
+            localStorage.setItem("idBookPage", rnd);
+            window.location.href = "http://localhost:63342/BlueLibrary/book.html";
+        },
+        error: function (data) {
+            console.log("Nu s-a reusit", data);
+        }
+    });
+}
+
+function createBook() {
+    $.ajax({
+        url: 'http://localhost:3000/book',
+        type: 'POST',
+        data: newBook,
+        success: function (data) {
+            console.log(data);
+            window.location.href = "http://localhost:63342/BlueLibrary/deals.html";
+        },
+        error: function (data) {
+            console.log("Nu s-a reusit", data);
+        }
+    });
+}
+
+function updateBook() {
+    $.ajax({
+        url: 'http://localhost:3000/book/0',
+        type: 'PUT',
+        data: newUpdateBook,
+        success: function (data) {
+            console.log(data);
+            window.location.href = "http://localhost:63342/BlueLibrary/deals.html";
+        },
+        error: function (data) {
+            console.log("Nu s-a reusit", data);
+        }
+    });
+}
+
+function deleteBook() {
+
+    var idVar = localStorage.getItem("dataLength");
+    $.ajax({
+        url: 'http://localhost:3000/book/' + idVar,
+        type: 'DELETE',
+        success: function (data) {
+            console.log(data);
+            window.location.href = "http://localhost:63342/BlueLibrary/deals.html";
+        },
+        error: function (data) {
+            console.log("Nu s-a reusit");
+        }
+    });
+}
+
+function getStock() {
+    $.ajax({
+        url: 'http://localhost:3000/book',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            for(let i = 0; i < data.length; i++)
+            {
+                data[i] = createElement(data[i]);
+            }
+
+            localStorage.setItem("dataLength", (data.length - 1).toString());
+        },
+        error: function(data) {
+            console.log("Nu s-a reusit", data);
+        }
+    });
+}
+
+function createElement(dataObject) {
+    var gallery = document.getElementsByClassName("gallery")[0];
+
+    /*console.log(dataObject);*/
+    var divImage = document.createElement("div");
+    var image = document.createElement("img");
+    var divText = document.createElement("div");
+    var h1Title = document.createElement("h1");
+    var pDescription = document.createElement("p");
+    var footerProduct = document.createElement("span");
+    var price = document.createElement("p");
+    var linkElement = document.createElement("a");
+
+    divImage.className = "image";
+
+    if(dataObject.shortDescription)
+    {
+        if (dataObject.thumbnailUrl)
+            image.src = dataObject.thumbnailUrl;
+        else
+            image.src = "images/No_Picture.jpg";
+        image.alt = "";
+
+        //console.log(image.width, image.height);
+
+        divText.className = "text";
+
+        h1Title.innerText = "About the product";
+        h1Title.className = "font-x1";
+
+        pDescription.classList.add("font-xs", "description");
+
+        res = dataObject.shortDescription.split(" ");
+        max = 20;
+
+        if (res.length > max)
+        {
+            for(let i = 0; i < max; i++)
+                pDescription.innerText += res[i] + " ";
+            pDescription.innerText += res[max] + "...";
+
+        }
+        else
+        {
+            for(let i = 0; i < res.length - 1; i++)
+                pDescription.innerText += res[i] + " ";
+
+            pDescription.innerText += res[res.length - 1] + "...";
+        }
+
+        footerProduct.className = "inline";
+
+        price.innerText = (dataObject.price).toString() + " $";
+
+
+        price.classList.add("font-xs", "fl_left", "bold");
+
+        linkElement.innerText = "More >>";
+        linkElement.classList.add("font-xs", "fl_left","bold");
+        linkElement.href = "http://localhost:63342/BlueLibrary/book.html";
+
+        linkElement.onmouseover = function(){localStorage.setItem("idBookPage", dataObject.id); console.log(dataObject.id);};
+
+        footerProduct.appendChild(price);
+        footerProduct.appendChild(linkElement);
+
+        divText.appendChild(h1Title);
+        divText.appendChild(pDescription);
+        divText.appendChild(footerProduct);
+
+        divImage.appendChild(image);
+        divImage.appendChild(divText);
+
+        gallery.appendChild(divImage);
+    }
+
+    return dataObject;
+    //console.log(linkElement.href)
+}
